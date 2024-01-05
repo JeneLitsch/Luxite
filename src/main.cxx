c7cf9ffd9c316b59a48e259f19f3bf2f5149def8
Sera K. Litsch <litsch.jene@gmail.com>
2024-01-05 13:31:34 +0100

    Added basic multithreading

--- src/main.cxx
+++ src/main.cxx
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <numbers>
 #include <tuple>
+#include <string_view>
+#include <chrono>
+#include <span>
+#include <iomanip>
+#include <future>
 
 #include "stdxx/matrix.hxx"
 #include "stdxx/log.hxx"
@@ -61,8 +66,27 @@ std::tuple<float, float, float> render_rec(std::size_t rec_counter, bool loose_e
 
 
 
-std::vector<std::uint8_t> render(stx::size2u size, const Scene & scene, const Camera & camera) {
+struct Options {
+	bool threaded = false;
+};
+
+
+
+Options parse_options(std::span<char *> rest) {
+	Options options;
+	for(char * option : rest) {
+		if(std::string_view{option} == "--threaded") {
+			options.threaded = true;
+		}
+	}
+	return options;
+}
+
+
+
+std::vector<std::uint8_t> render(const stx::size2u resolution, const Scene & scene, const Camera & camera, const Options & options) {
 	std::vector<std::uint8_t> data;
+	data.resize(resolution.x * resolution.y * 4);
 	const float fov = 45.f;
 	const stx::position3f start = camera.position;
 	const stx::quatf orientation = camera.rotation;
@@ -70,23 +94,50 @@ std::vector<std::uint8_t> render(stx::size2u size, const Scene & scene, const Ca
 	constexpr static std::size_t max_bounce = 4;
 	constexpr static std::size_t split = 3;
 
-	for(std::int32_t y = 0; y < size.y; ++y){
-		for(std::int32_t x = 0; x < size.x; ++x){
-			float dx = (static_cast<float>(x) / static_cast<float>(size.x)) * 2.f - 1.f;
-			float dy = (static_cast<float>(y) / static_cast<float>(size.y)) * 2.f - 1.f;
-			const stx::vector3f default_dir {dx, 1, -dy};
-			const stx::vector3f dir = stx::dim_cast<3>(stx::matrix4f::from_quat(camera.rotation) * stx::dim_cast<4>(default_dir));
-			auto [r,g,b] = render_rec(max_bounce, false, split, scene, start, dir);
-			data.push_back(static_cast<std::uint8_t>(std::clamp(r * 255.f, 0.f, 255.f)));
-			data.push_back(static_cast<std::uint8_t>(std::clamp(g * 255.f, 0.f, 255.f)));
-			data.push_back(static_cast<std::uint8_t>(std::clamp(b * 255.f, 0.f, 255.f)));
-			data.push_back(255);
+	const auto f = [&resolution, &scene, &camera, &start, &data](std::int32_t y_start, std::int32_t y_end) {
+		for(std::int32_t y = y_start; y < y_end; ++y){
+			for(std::int32_t x = 0; x < resolution.x; ++x){
+				const float dx = (static_cast<float>(x) / static_cast<float>(resolution.x)) * 2.f - 1.f;
+				const float dy = (static_cast<float>(y) / static_cast<float>(resolution.y)) * 2.f - 1.f;
+				const stx::vector3f default_dir {dx, 1, -dy};
+				const stx::vector3f dir = stx::dim_cast<3>(stx::matrix4f::from_quat(camera.rotation) * stx::dim_cast<4>(default_dir));
+				const auto [r,g,b] = render_rec(max_bounce, false, split, scene, start, dir);
+				const std::size_t i = 4 * (y * resolution.x + x);
+				data[i + 0] = static_cast<std::uint8_t>(std::clamp(r * 255.f, 0.f, 255.f));
+				data[i + 1] = static_cast<std::uint8_t>(std::clamp(g * 255.f, 0.f, 255.f));
+				data[i + 2] = static_cast<std::uint8_t>(std::clamp(b * 255.f, 0.f, 255.f));
+				data[i + 3] = 255;
+			}
+			if(y % (resolution.y / 20) == 0) {
+				const float percentage = (static_cast<float>(y - y_start) / (y_end - y_start)) * 100;
+				std::cout 
+					<< std::round(percentage) << "% in chunk"
+					<< "["<< y_start << ", " << y_end << "]"
+					<< "\n";
+			} 
 		}
-		if(y % 10 == 0) {
-			std::cout << (static_cast<float>(y) / size.y) * 100 << "%" << "\n";
-		} 
+	};
+
+	if(options.threaded) {
+		constexpr static std::size_t num_of_threads = 4;
+		std::array<std::future<void>, num_of_threads> chunks;
+
+		for(std::size_t i = 0; i < num_of_threads; ++i) {
+			chunks[i] = std::async(std::launch::async, [&, i] () {
+				const std::int32_t y_start = resolution.y * i / num_of_threads;
+				const std::int32_t y_end   = resolution.y * (i + 1) / num_of_threads;
+				return f(y_start, y_end);
+			});
+		}
+
+		for(const std::future<void> & chunk : chunks) {
+			chunk.wait();
+		}
 	}
-	std::cout << "100%" << "\n";
+	else {
+		f(0, resolution.y);
+	}
+
 	return data;
 }
 
@@ -94,6 +145,7 @@ std::vector<std::uint8_t> render(stx::size2u size, const Scene & scene, const Ca
 
 int main(int argc, char ** argv) {
 	stx::log.register_output(std::cout);
+
 
 	if(argc < 4) {
 		stx::log[stx::ERROR] 
@@ -103,14 +155,16 @@ int main(int argc, char ** argv) {
 	}
 
     const std::filesystem::path in_path {argv[1]};
+	const std::string config = argv[2];
     const std::filesystem::path out_path {argv[3]};
-	
+	const Options options = parse_options(std::span<char*>{argv + 4, argv + argc});
+
     const stx::json::node data = stx::json::from_file(in_path/"manifest.json");
     const stx::json::iterator manifest {data};
 
 	const Scene scene = load_scene(in_path, manifest);
-	const stx::size2u resolution = load_resolution(manifest, argv[2]);
-	const Camera camera = load_camera(manifest, argv[2]);
+	const stx::size2u resolution = load_resolution(manifest, config);
+	const Camera camera = load_camera(manifest, config);
 
 	stx::log[stx::WRITE] << "Luxite: Voxel Raytracer (c) 2024 Sera K. Litsch ";
 
@@ -125,6 +179,11 @@ int main(int argc, char ** argv) {
 	stx::log[stx::WRITE] << "Quaternion: " << camera.rotation;
 	stx::log.indent_out();
 
+	stx::log[stx::INFO] << "Options";
+	stx::log.indent_in();
+	stx::log[stx::WRITE] << "--threaded: " << std::boolalpha << options.threaded;
+	stx::log.indent_out();
+
 	stx::log[stx::INFO] << "Output";
 	stx::log.indent_in();
 	stx::log[stx::WRITE] << "Format:     " << "PNG";
@@ -132,8 +191,13 @@ int main(int argc, char ** argv) {
 	stx::log.indent_out();
 
 	stx::log[stx::INFO] << "Rendering...";
-	auto rendered_image = render(resolution, scene, camera);
-	stx::log[stx::INFO] << "Renering done";
+	std::chrono::steady_clock clock;
+	std::chrono::time_point time_start = clock.now();
+	auto rendered_image = render(resolution, scene, camera, options);
+	std::chrono::time_point time_end= clock.now();
+	std::chrono::duration<double> duration = std::chrono::duration_cast<std::chrono::duration<double>>(time_end - time_start);
+	stx::log[stx::INFO] << "Renering done. Duration: " << duration;
+
 
 	stx::log[stx::INFO] << "Writing image...";
 	if(std::filesystem::create_directory(out_path.parent_path())) {
